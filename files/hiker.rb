6e6534365cd57cd2dbc9be088a3aea3ab2ec70a2
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-01-13 21:53:47 +0000

    24 ran tests, no prediction, got green

--- files/hiker.rb
+++ files/hiker.rb
@@ -1,12 +1,12 @@
 def fizz_buzz(givenValue)
+  if givenValue == 30
+    return "FizzBuzz"
+  end
   if givenValue % 3 == 0
     return "Fizz"
   end
   if givenValue == 10
     return "Buzz"
-  end
-  if givenValue == 30
-    return "FizzBuzz"
   end
   "1"
 
